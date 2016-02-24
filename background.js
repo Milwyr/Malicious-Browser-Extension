5c0b1c6fc28a8bf3a77af641ea0d7f96a32d3211
Milton <tropa9527@gmail.com>
2016-02-24 23:33:48 +0000

    Initial commit
    The code does not do anything meaningful now.

---
+++ background.js
@@ -0,0 +1,10 @@
+/*
+Author: Milton Li 2015-2016, Newcastle University
+Project: Attack on Helios voting system
+*/
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    switch(request.type) {
+    }
+  }
+);
---
+++ contentScript.js
@@ -0,0 +1,11 @@
+/*
+Author: Milton Li 2015-2016, Newcastle University
+Project: Attack on Helios voting system
+*/
+
+var userId;
+var userChoices;
+
+$(document).ready(function() {
+  
+});
